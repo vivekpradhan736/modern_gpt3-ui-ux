e2617446fe6d87ac283d5c0b0d7f4d4bf937eccd
Vivek <vivekpradhan736@gmail.com>
2023-04-26 23:15:01 +0530

    footer updated

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => (
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2023 GPT-3. All rights reserved.</p>
+      <p>@2022 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
