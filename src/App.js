58af786753850a5d27cee6860c1652de796f405c
Vivek <vivekpradhan736@gmail.com>
2024-07-30 20:06:09 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@ import { FaCoffee } from 'react-icons/fa';
 const App = () => {
   return (
     <>
-      <Button className="mx-7 my-4" size="large" variant="primary" borderRadius="rounded-sm" icon={FaCoffee} iconPosition="left" enabled={true}  loading={true} >
+      <Button className="mx-7 my-4" size="large" variant="primary" borderRadius="rounded-sm" icon={FaCoffee} iconPosition="left"   loading={true} >
         Primary
       </Button>
       <Button className="mx-7 my-4" textColor="red" size="large" variant="secondary" borderRadius="rounded-xs" icon={FaCoffee} iconPosition="right" enabled={true} hoverEffect="fill-left-to-right">
         Secondary
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="success" borderRadius="rounded-lg" enabled={true} hoverEffect="scale">
+      <Button className="mx-7 my-4"  enabled={false}  variant="success" borderRadius="rounded-lg"    hoverEffect="scale">
         Success
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="danger" borderRadius="rounded-md" enabled={true}   >
+      <Button className="mx-7 my-4"    variant="danger" borderRadius="rounded-md"      >
         Danger
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="warning" borderRadius="rounded-full" enabled={true}   >
+      <Button className="mx-7 my-4"    variant="warning" borderRadius="rounded-full"      >
         Warning
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="info" borderRadius="rounded-none" enabled={true}   >
+      <Button className="mx-7 my-4"  variant="info" borderRadius="rounded-none"      >
         Info
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="light" borderRadius="rounded-lg" enabled={true}   >
+      <Button className="mx-7 my-4"  textColor="red"  variant="light" borderRadius="rounded-lg"      >
         Light
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="dark" borderRadius="rounded-lg" enabled={true}   >
+      <Button className="mx-7 my-4"    variant="dark" borderRadius="rounded-lg"      >
         Dark
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="outline" borderRadius="rounded-lg" enabled={true}   >
+      <Button className="mx-7 my-4"  textColor="red"  variant="outline" borderRadius="rounded-lg"      >
         Outline
       </Button>
-      <Button className="mx-7 my-4" size="large" variant="gradient" gradient borderRadius="rounded-md" enabled={true}   >
+      <Button className="mx-7 my-4"    variant="gradient" gradient borderRadius="rounded-md"      >
         Gradient
       </Button>
-      <Button className="mx-7 my-4" customWidth="200px" customHeight="60px" borderRadius="rounded-full" variant="primary" icon={FaCoffee} iconPosition="left" enabled={true} hoverText="Hovered">
+      <Button className="mx-7 my-4" customWidth="200px" customHeight="60px" borderRadius="rounded-full" variant="primary" icon={FaCoffee} iconPosition="left"    hoverText="Hovered">
         Not Hover
       </Button>
     </>
